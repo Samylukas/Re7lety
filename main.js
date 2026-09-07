92ef0013e38d6c54632a042723afde29e76fa5ed
Samylukas <144998127+Samylukas@users.noreply.github.com>
2026-09-07 12:51:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // =========================================================================
 
 // الرابط الجديد الخاص بك من خطوة Deployment الأخيرة
-const API_URL = "AKfycbwr-LoCPIygEglH2Kvpo5l7sqRKtQm73ilrgBkiiBqhNZuyaxZVu9wD5D9wozB0xp-2Zg";
+const API_URL = "https://script.google.com/a/macros/marsa-alam-airport.com/s/AKfycbwr-LoCPIygEglH2Kvpo5l7sqRKtQm73ilrgBkiiBqhNZuyaxZVu9wD5D9wozB0xp-2Zg/exec";
 
 let allTrips = [];
 let currentUser = null;
